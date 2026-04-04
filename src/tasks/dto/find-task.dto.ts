414e01d7a8a8b989c0e807f8ace134285887b0aa
Lucas Sanginetto <lucassanginetto@protonmail.com>
2026-04-04 17:49:27 -0300

    feat: use NestJS CLI Swagger plugin to automatically detect DTO properties

--- src/tasks/dto/find-task.dto.ts
+++ src/tasks/dto/find-task.dto.ts
@@ -1,12 +1,22 @@
 import { Task, TaskStatus } from '../entities/task.entity';
 
 export class FindTaskDto {
+  public id: number;
+  public title: string;
+  public description: string;
+  public status: TaskStatus;
+
   constructor(
-    public id: number,
-    public title: string,
-    public description: string,
-    public status: TaskStatus,
-  ) {}
+    id: number,
+    title: string,
+    description: string,
+    status: TaskStatus,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+  }
 
   static fromTask(this: void, task: Task): FindTaskDto {
     return new FindTaskDto(task.id, task.title, task.description, task.status);
--- src/users/dto/find-user.dto.ts
+++ src/users/dto/find-user.dto.ts
@@ -1,10 +1,13 @@
 import { User } from '../entities/user.entity';
 
 export class FindUserDto {
-  constructor(
-    public id: number,
-    public username: string,
-  ) {}
+  public id: number;
+  public username: string;
+
+  constructor(id: number, username: string) {
+    this.id = id;
+    this.username = username;
+  }
 
   static fromUser(this: void, user: User): FindUserDto {
     return new FindUserDto(user.id, user.username);
